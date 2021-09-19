7a5b52ec299f1ac7f3aaefb9a5449b3aa2e81ce4
whyudin4 <wahyu.udinnn1@gmail.com>
2021-09-19 12:02:42 +0700

    initial commit

---
+++ array.js
@@ -0,0 +1,36 @@
+const namaMantan = ["Olip", "Sabian", "Bibu"]
+
+namaMantan.forEach( function(item, index, array) {
+    console.log(item, index);
+})
+
+namaMantan.push("dewi");
+
+// console.log(namaMantan);
+
+// namaMantan.pop();
+
+// console.log(namaMantan);
+
+// namaMantan.shift();
+
+// console.log(namaMantan);
+
+// console.log(namaMantan.indexOf("Bibu")+1);
+
+// const indexOlip = namaMantan.indexOf("Olip");
+
+// console.log(indexOlip);
+// namaMantan.splice(indexOlip, 1);
+
+// console.log(namaMantan);
+
+const namaMantanLama = namaMantan.slice();
+
+namaMantan.pop();
+
+
+
+console.log("Nama mantan: ", namaMantan);
+
+console.log("Nama mantan lama: ", namaMantanLama);
---
+++ var.js
@@ -0,0 +1,4 @@
+const sayaSukaEsKrim = "sangat suka dong";
+const sayaSukaKamu = "apalagi kamu";
+
+console.log(`${sayaSukaKamu} ${sayaSukaEsKrim}`);
